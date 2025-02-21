661945909a136959f6d4b2a67ff9e91da45d3a96
MatteoAjanaku <matteoajanaku@gmail.com>
2025-02-21 18:30:49 +0000

    update gitignore and add cube placement
    cube placement
    
    cleanup git and add cube placement
    TODO: bug with raycasts going through the buttons
    
    update gitignore
    
    update gitignore
    
    update gitignore
    
    update gitignore
    
    gitignore update
    
    The other gitignore had some rich text formatting that wasnt working on my computer
    
    Removed .gitignore from tracking

--- Assets/Scripts/PlaceGolfObjects.cs
+++ Assets/Scripts/PlaceGolfObjects.cs
@@ -1,17 +1,18 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 // Define placement modes.
-public enum PlacementMode { None, PlacingFlag, PlacingGolf }
+public enum PlacementMode { None, PlacingFlag, PlacingGolf, PlacingCube }
 
 public class PlaceGolfObjects : MonoBehaviour
 {
     [Header("Prefabs")]
     [SerializeField] private GameObject holePrefab;   // The flag/hole prefab.
     [SerializeField] private GameObject ballPrefab;   // The golf ball prefab.
-
+    [SerializeField] private GameObject cubePrefab;   // cube obstacle prefab
     private ARRaycastManager _arRaycastManager;
     private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
 
@@ -19,6 +20,8 @@ public class PlaceGolfObjects : MonoBehaviour
     private GameObject placedHole;
     private GameObject placedBall;
 
+    private GameObject placedCube;
+    private List<GameObject> cubeList = new List<GameObject>();
     // Current placement mode.
     public PlacementMode currentPlacementMode = PlacementMode.None;
 
@@ -28,7 +31,9 @@ public class PlaceGolfObjects : MonoBehaviour
     public GameObject golfButton;
     public GameObject resetButton;
     public GameObject goButton;
+    public GameObject cubeButton;
 
+    private Boolean flagButtonPressed,golfButtonPressed,cubeButtonPressed;
     private bool isDragging = false;
 
     private void Awake()
@@ -41,7 +46,10 @@ public class PlaceGolfObjects : MonoBehaviour
         // Only process placement if we're in a placement mode.
         if (currentPlacementMode == PlacementMode.None) return;
         if (Input.touchCount == 0) return;
-
+        if (flagButtonPressed || golfButtonPressed || cubeButtonPressed){
+            flagButtonPressed=golfButtonPressed=cubeButtonPressed=false;
+            return;
+        }
         Touch touch = Input.GetTouch(0);
 
         // Raycast into the AR scene.
@@ -65,6 +73,11 @@ public class PlaceGolfObjects : MonoBehaviour
                         placedBall = Instantiate(ballPrefab, hitPose.position,
                             Quaternion.Euler(0, hitPose.rotation.eulerAngles.y, 0));
                     }
+                    else if (currentPlacementMode == PlacementMode.PlacingCube)
+                    {
+                        placedCube = Instantiate(cubePrefab, hitPose.position,
+                            Quaternion.Euler(0,hitPose.rotation.eulerAngles.y,0));
+                    }
                     isDragging = true;
                 }
                 else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
@@ -80,10 +93,16 @@ public class PlaceGolfObjects : MonoBehaviour
                         placedBall.transform.position = hitPose.position;
                         placedBall.transform.rotation = Quaternion.Euler(0, hitPose.rotation.eulerAngles.y, 0);
                     }
+                    else if (currentPlacementMode == PlacementMode.PlacingCube && placedCube != null)
+                    {
+                        placedCube.transform.position = hitPose.position;
+                        placedCube.transform.rotation = Quaternion.Euler(0, hitPose.rotation.eulerAngles.y, 0);
+                    }
                 }
                 else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
                 {
-                    // Finalize placement: hide the corresponding button.
+                    // Finalize placement: hide the corresponding button. 
+                    // Add the placedCube to the cubeList so more can be added
                     if (currentPlacementMode == PlacementMode.PlacingFlag)
                     {
                         if (flagButton != null)
@@ -94,19 +113,26 @@ public class PlaceGolfObjects : MonoBehaviour
                         if (golfButton != null)
                             golfButton.SetActive(false);
                     }
+                    else if (currentPlacementMode == PlacementMode.PlacingCube)
+                    {
+                        cubeList.Add(placedCube);
+                        //placedCube = null;                        
+                    }
                     // Reset the mode so the user can choose the other item.
                     currentPlacementMode = PlacementMode.None;
                     isDragging = false;
 
-                    // If both objects are placed, show the Go button.
+                    // If flag and ball placed, show the Go button
                     if (placedHole != null && placedBall != null)
                     {
+
                         if (goButton != null)
                             goButton.SetActive(true);
                     }
                 }
             }
         }
+        flagButtonPressed=golfButtonPressed=cubeButtonPressed=false;
     }
 
     // Check if the ARRaycastHit represents a nearly horizontal surface.
@@ -120,22 +146,40 @@ public class PlaceGolfObjects : MonoBehaviour
     // Called when the user taps the Flag button.
     public void OnFlagButtonPressed()
     {
+        flagButtonPressed = true;
         currentPlacementMode = PlacementMode.PlacingFlag;
+
     }
 
     // Called when the user taps the Golf button.
     public void OnGolfButtonPressed()
     {
+        flagButtonPressed = true;
         currentPlacementMode = PlacementMode.PlacingGolf;
     }
 
+    public void OnCubeButtonPressed()
+    {
+        cubeButtonPressed = true;
+        currentPlacementMode = PlacementMode.PlacingCube;
+        cubeList.Add(placedCube);
+        placedCube = null;
+    } 
     // Called when the user taps the Reset button.
     public void ResetPlacement()
     {
         if (placedHole != null) Destroy(placedHole);
         if (placedBall != null) Destroy(placedBall);
+        foreach (GameObject cube in cubeList){
+            if (cube!= null){
+                Destroy(cube);
+            }
+            
+        }
+        cubeList.Clear();
         placedHole = null;
         placedBall = null;
+        placedCube = null;
         currentPlacementMode = PlacementMode.None;
 
         // Show the placement buttons again and hide the Go button.
@@ -143,8 +187,12 @@ public class PlaceGolfObjects : MonoBehaviour
             flagButton.SetActive(true);
         if (golfButton != null)
             golfButton.SetActive(true);
+        if (cubeButton != null){
+            cubeButton.SetActive(true);
+        }
         if (goButton != null)
             goButton.SetActive(false);
+
     }
 
     // Called when the user taps the Go button.
@@ -159,5 +207,10 @@ public class PlaceGolfObjects : MonoBehaviour
             goButton.SetActive(false);
         }
         // Optionally hide the UI or disable further placement.
+        currentPlacementMode = PlacementMode.None;
+        cubeButton.SetActive(false);
     }
 }
+//TODO: bug when you press one button then another
+//probably because raycast going through button
+//might be best to handle all touches in one script
